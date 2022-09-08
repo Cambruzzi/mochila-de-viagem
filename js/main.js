47f3132156f92428eb1ddfc5208e970037d12eb2
Cambruzzi <leocfritz@gmail.com>
2022-09-08 17:04:14 -0300

    teste

--- js/main.js
+++ js/main.js
@@ -1,79 +1,84 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []
+const itens = JSON.parse(localStorage.getItem("itens") ) || []
 
-itens.forEach( (elemento) => {
+itens.forEach(elemento => {
     criaElemento(elemento)
-} )
+});
+
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
-
-    const existe = itens.find( elemento => elemento.nome === nome.value )
-
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    if (existe) {
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+
+    if (existe){
         itemAtual.id = existe.id
-        
-        atualizaElemento(itemAtual)
+        atualizarElemento(itemAtual)
+        itens[itens.findIdex(elemento => elemento.id === existe.id)] = itemAtual
 
-        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
-    } else {
-        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
-
+    }else{
+        itemAtual.id = itens[itens.length-1] ? (itens[itens.length-1].id + 1 ) : 0
         criaElemento(itemAtual)
-
         itens.push(itemAtual)
     }
 
-    localStorage.setItem("itens", JSON.stringify(itens))
+
+ 
+
+    localStorage.setItem("itens",JSON.stringify(itens))
+
 
     nome.value = ""
     quantidade.value = ""
+
+    
+
+
+
 })
 
-function criaElemento(item) {
-    const novoItem = document.createElement("li")
+function criaElemento(itemAtual){
+
+
+    const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
-    const numeroItem = document.createElement("strong")
-    numeroItem.innerHTML = item.quantidade
-    numeroItem.dataset.id = item.id
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = itemAtual.quantidade
+    numeroItem.dataset.id = itemAtual.id
     novoItem.appendChild(numeroItem)
-    
-    novoItem.innerHTML += item.nome
-
-    novoItem.appendChild(botaoDeleta(item.id))
+    novoItem.innerHTML += itemAtual.nome
+    novoItem.appendChild(botaoDeleta(itemAtual.id))
 
     lista.appendChild(novoItem)
+
 }
 
-function atualizaElemento(item) {
-    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+function atualizarElemento (itemAtual){
+    document.querySelector("[data-id = '"+itemAtual.id+"']").innerHTML = itemAtual.quantidade
 }
 
-function botaoDeleta(id) {
+function botaoDeleta(id){
     const elementoBotao = document.createElement("button")
     elementoBotao.innerText = "X"
-
-    elementoBotao.addEventListener("click", function() {
+    elementoBotao.addEventListener("click", function () {
         deletaElemento(this.parentNode, id)
     })
-
-    return elementoBotao
+    return  elementoBotao
 }
 
 function deletaElemento(tag, id) {
     tag.remove()
 
-    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1 )
 
-    localStorage.setItem("itens", JSON.stringify(itens))
+    localStorage.setItem("itens",JSON.stringify(itens))
 }
